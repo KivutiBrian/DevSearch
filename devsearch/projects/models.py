925d214054e79a276083b4e15b2405605c4ca8b7
Gaideh <briankivuti1@gmail.com>
2022-03-21 02:07:23 +0300

    Project, Review & Tag models setup

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,48 @@
+from operator import mod
+import uuid
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #blank allows the form to be empty
+    demo_link = models.CharField(max_length=2000, null=True, blank=True) #blank allows the form to be empty
+    source_link = models.CharField(max_length=2000, null=True, blank=True) #blank allows the form to be empty
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    public_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False) #editable - means no one can edit this.
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Review(models.Model):
+
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+
+    # owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # models.CASCADE will delete all the review 
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    public_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False) #editable - means no one can edit this.
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    public_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False) #editable - means no one can edit this.
+
+    def __str__(self) -> str:
+        return self.name
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+projects_list = [
+    {
+        "id":"1",
+        "title": "Kendely Project",
+        "description": "An oil management platform"
+    },
+    {
+        "id":"2",
+        "title": "Msanii",
+        "description": "Connect to artists and make bookings"
+    },
+    {
+        "id":"3",
+        "title": "Muer Solutions",
+        "description": "A software company specializing in IT solutions"
+    },
+] 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+
+    msg = "Hello you are on the project page"
+    age = 10
+
+    context = {
+        "message": msg,
+        "age": age,
+        "project_list": projects_list
+    }
+    return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+
+    projectObj = None
+    for i in projects_list:
+        if i['id'] == pk:
+            projectObj = i
+    
+    context = {
+        "project": projectObj
+    }
+    return render(request, 'projects/single-project.html', context)
